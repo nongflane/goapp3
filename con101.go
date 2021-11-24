f523ccfe7e5a2b6a35aa3ee1d34e8680ff2f26d6
nongflane <69183471+nongflane@users.noreply.github.com>
2021-11-24 12:50:35 +0700

    Add files via upload

---
+++ con101.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	isAdmin := true
+	canUpdate := true
+
+	if isAdmin == true {
+		fmt.Println("Welcome,Admin")
+		if canUpdate == true {
+			fmt.Println("Can Update")
+		}
+	}
+}
---
+++ con106.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a bool = true //Long Type
+	var b int = 5 //Long Type
+	c := 3.14 //Short Type
+	d := "Hi!" //Short Type
+
+	fmt.Println("Boolaen: ",a);
+	fmt.Println("integer: ",b);
+	fmt.Println("Float: ",c);
+	fmt.Println("String: ",d);
+}
